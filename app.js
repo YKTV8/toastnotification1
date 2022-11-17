59346174e62ad4a58b25a36abbfa2faaf1a7468f
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-11-17 00:46:16 -0800

    Notication 11/17
    Start code for HTML, CSS and JS

---
+++ app.js
@@ -0,0 +1,17 @@
+const button = document.getElementsByClassName('.btn')
+
+const toasts = document.getElementById('toasts')
+
+const messages = [
+    'mess 1',
+    'mess 2', 
+    'mess 3',
+]
+
+// button.addEventListener('click', () => createNotice()
+// )
+
+addEventListener
+function createNotice(){
+    console.log(123)
+}
